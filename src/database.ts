42f02eee66a67e6363b05469db42350f1548f017
Samuel Anozie <samuelanozie10@yahoo.com>
2022-12-29 16:07:53 +0100

    fix: specified different database for testing

--- src/database.ts
+++ src/database.ts
@@ -2,14 +2,35 @@ import dotenv from 'dotenv';
 import { Pool } from 'pg';
 
 dotenv.config();
-const { POSTGRES_HOST, DEV_POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } =
-    process.env;
 
-const client = new Pool({
-    host: POSTGRES_HOST,
-    database: DEV_POSTGRES_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-});
+const {
+    POSTGRES_HOST,
+    POSTGRES_DB,
+    POSTGRES_USER,
+    POSTGRES_PASSWORD,
+    POSTGRES_TEST_DB,
+    ENV,
+} = process.env;
+
+let client;
+console.log(ENV);
+
+if (ENV === 'test') {
+    client = new Pool({
+        host: POSTGRES_HOST,
+        database: POSTGRES_TEST_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
+    });
+}
+
+if (ENV === 'dev') {
+    client = new Pool({
+        host: POSTGRES_HOST,
+        database: POSTGRES_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
+    });
+}
 
 export default client;
