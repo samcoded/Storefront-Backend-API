74745494b45759621f5a3fb473a2eecd936712c9
Samuel Anozie <samuelanozie10@yahoo.com>
2022-12-29 16:08:16 +0100

    fix: seperate route from app

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
+import userRoutes from './handlers/user';
+import productRoutes from './handlers/product';
+import orderRoutes from './handlers/order';
+
 const app: express.Application = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!');
 });
+
+userRoutes(app);
+productRoutes(app);
+orderRoutes(app);
 
 export default app;
