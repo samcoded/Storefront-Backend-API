a910e77dd06e55fd58659b436de5f8af99c78c95
Samuel Anozie <samuelanozie10@yahoo.com>
2022-12-29 12:10:15 +0100

    feat: Added migrations

---
+++ migrations/sqls/20221229110320-users-up.sql
@@ -0,0 +1,9 @@
+/* Replace with your SQL commands */
+
+CREATE TABLE users (
+  id              SERIAL PRIMARY KEY,
+  username        VARCHAR(250) NOT NULL,
+  firstname       VARCHAR(250) NOT NULL,
+  lastname        VARCHAR(250) NOT NULL,
+  password_digest VARCHAR(250) NOT NULL
+);
