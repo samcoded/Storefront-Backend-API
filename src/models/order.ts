8318fa129d383d99dfae7713f88d51ed43c51392
Samuel Anozie <samuelanozie10@yahoo.com>
2022-12-30 19:29:55 +0100

    feat: test for order model

--- src/models/order.ts
+++ src/models/order.ts
@@ -1,9 +1,12 @@
 // @ts-ignore
 import Client from '../database';
+import { Product, ProductStore } from '../models/product';
 
 export interface OrderProduct {
     product_id: number;
     quantity: number;
+    name?: string;
+    price?: number;
 }
 
 export interface Order {
@@ -23,7 +26,7 @@ export class OrderStore {
             const { rows } = await connection.query(sql);
 
             const orderProductsSql =
-                'SELECT product_id, quantity FROM order_products WHERE order_id=($1)';
+                'SELECT product_id, quantity, name, price FROM order_products JOIN products ON order_products.product_id=products.id WHERE order_id=($1)';
             const orders = [];
 
             for (const order of rows) {
@@ -69,6 +72,15 @@ export class OrderStore {
                     quantity,
                 ]);
 
+                const productSql = 'SELECT * FROM products WHERE id=($1)';
+                const prodDetails = await connection.query(productSql, [
+                    product_id,
+                ]);
+
+                if (prodDetails.rows.length > 0) {
+                    rows[0].name = prodDetails.rows[0].name;
+                    rows[0].price = prodDetails.rows[0].price;
+                }
                 orderProducts.push(rows[0]);
             }
 
@@ -94,7 +106,7 @@ export class OrderStore {
             const order = rows[0];
 
             const orderProductsSql =
-                'SELECT product_id, quantity FROM order_products WHERE order_id=($1)';
+                'SELECT product_id, quantity, name, price FROM order_products JOIN products ON order_products.product_id=products.id WHERE order_id=($1)';
             const { rows: orderProductRows } = await connection.query(
                 orderProductsSql,
                 [id]
@@ -150,7 +162,7 @@ export class OrderStore {
         }
     }
 
-    async deleteOrder(id: number): Promise<Order> {
+    async delete(id: number): Promise<Order> {
         try {
             // @ts-ignore
             const connection = await Client.connect();
@@ -167,6 +179,65 @@ export class OrderStore {
             return order;
         } catch (err) {
             throw new Error(`Could not delete order ${id}. ${err}`);
+        }
+    }
+
+    async getCurrentOrders(id: number) {
+        try {
+            const sql =
+                'SELECT * FROM orders WHERE user_id=($1) AND status=true';
+            // @ts-ignore
+            const connection = await Client.connect();
+            const { rows } = await connection.query(sql, [id]);
+            const order = rows[0];
+
+            const orderProductsSql =
+                'SELECT product_id, quantity, name, price FROM order_products JOIN products ON order_products.product_id=products.id WHERE order_id=($1)';
+            const { rows: orderProductRows } = await connection.query(
+                orderProductsSql,
+                [order.id]
+            );
+
+            connection.release();
+
+            return {
+                ...order,
+                products: orderProductRows,
+            };
+        } catch (err) {
+            throw new Error(`Could not find order ${id}. ${err}`);
+        }
+    }
+
+    async getCompleteOrders(id: number) {
+        try {
+            const sql =
+                'SELECT * FROM orders WHERE user_id=($1) AND status=false';
+            // @ts-ignore
+            const connection = await Client.connect();
+
+            const { rows } = await connection.query(sql, [id]);
+
+            const orderProductsSql =
+                'SELECT product_id, quantity, name, price FROM order_products JOIN products ON order_products.product_id=products.id WHERE order_id=($1)';
+            const orders = [];
+
+            for (const order of rows) {
+                const { rows: orderProductRows } = await connection.query(
+                    orderProductsSql,
+                    [order.id]
+                );
+                orders.push({
+                    ...order,
+                    products: orderProductRows,
+                });
+            }
+
+            connection.release();
+
+            return orders;
+        } catch (err) {
+            throw new Error(`Could not get user orders. ${err}`);
         }
     }
 }
---
+++ src/tests/models/order.spec.ts
@@ -0,0 +1,203 @@
+import { Order, OrderStore } from '../../models/order';
+import { User, UserStore } from '../../models/user';
+import { Product, ProductStore } from '../../models/product';
+
+const orderStore = new OrderStore();
+
+describe('Order Model', () => {
+    const userStore = new UserStore();
+    const productStore = new ProductStore();
+
+    let order: Order,
+        userId: number,
+        productId: number,
+        retrievedOrder: Order,
+        productData: Product;
+
+    async function createOrder(order: Order) {
+        return orderStore.create(order);
+    }
+
+    async function deleteOrder(id: number) {
+        return orderStore.delete(id);
+    }
+
+    beforeAll(async () => {
+        const user: User = await userStore.create({
+            username: 'JakeDoe',
+            firstname: 'Jake',
+            lastname: 'Doe',
+            password: 'password123',
+        });
+
+        userId = user.id as number;
+        productData = {
+            name: 'Havells fan',
+            price: 2000,
+        };
+        const product: Product = await productStore.create(productData);
+
+        productId = product.id as number;
+
+        order = {
+            user_id: userId,
+            status: true,
+            products: [
+                {
+                    product_id: productId,
+                    quantity: 6,
+                },
+            ],
+        };
+
+        retrievedOrder = {
+            user_id: userId,
+            status: true,
+            products: [
+                {
+                    product_id: productId,
+                    quantity: 6,
+                    ...productData,
+                },
+            ],
+        };
+    });
+
+    afterAll(async () => {
+        await userStore.delete(userId);
+        await productStore.delete(productId as number);
+    });
+
+    it('Index method is present', () => {
+        expect(orderStore.index).toBeDefined();
+    });
+
+    it('Show method is present', () => {
+        expect(orderStore.read).toBeDefined();
+    });
+
+    it('Create method is present', () => {
+        expect(orderStore.create).toBeDefined();
+    });
+
+    it('Update method is present', () => {
+        expect(orderStore.update).toBeDefined();
+    });
+
+    it('Delete method is present', () => {
+        expect(orderStore.delete).toBeDefined();
+    });
+
+    it('Current Orders method is present', () => {
+        expect(orderStore.getCurrentOrders).toBeDefined();
+    });
+
+    it('Completed Orders method is present', () => {
+        expect(orderStore.getCompleteOrders).toBeDefined();
+    });
+
+    it('Create method should add a order', async () => {
+        const createdOrder: Order = await createOrder(order);
+
+        expect(createdOrder).toEqual({
+            id: createdOrder.id,
+            ...retrievedOrder,
+        });
+
+        await deleteOrder(createdOrder.id as number);
+    });
+
+    it('Index method should return a list of orders', async () => {
+        const createdOrder: Order = await createOrder(order);
+        const orderList = await orderStore.index();
+
+        expect(orderList).toEqual([createdOrder]);
+
+        await deleteOrder(createdOrder.id as number);
+    });
+
+    it('Show method should return the correct orders', async () => {
+        const createdOrder: Order = await createOrder(order);
+        const orderFromDb = await orderStore.read(createdOrder.id as number);
+
+        expect(orderFromDb).toEqual(createdOrder);
+
+        await deleteOrder(createdOrder.id as number);
+    });
+
+    it('Update method should update the order', async () => {
+        const createdOrder: Order = await createOrder(order);
+        const newOrderData: Order = {
+            products: [
+                {
+                    product_id: productId,
+                    quantity: 200,
+                },
+            ],
+            user_id: userId,
+            status: false,
+        };
+
+        const { products, status } = await orderStore.update(
+            createdOrder.id as number,
+            newOrderData
+        );
+
+        expect(products).toEqual(newOrderData.products);
+        expect(status).toEqual(newOrderData.status);
+
+        await deleteOrder(createdOrder.id as number);
+    });
+
+    it('Delete method should remove the order', async () => {
+        const createdOrder: Order = await createOrder(order);
+
+        await deleteOrder(createdOrder.id as number);
+
+        const orderList = await orderStore.index();
+
+        expect(orderList).toEqual([]);
+    });
+
+    it('Current Order method should return the correct orders', async () => {
+        const createdOrder: Order = await createOrder(order);
+        const orderList = await orderStore.getCurrentOrders(userId);
+        expect(orderList).toEqual(createdOrder);
+
+        await deleteOrder(createdOrder.id as number);
+    });
+
+    it('Completed Order method should return the correct orders', async () => {
+        const createdOrder: Order = await createOrder(order);
+        const newOrderData: Order = {
+            products: [
+                {
+                    product_id: productId,
+                    quantity: 200,
+                },
+            ],
+            user_id: userId,
+            status: false,
+        };
+
+        await orderStore.update(createdOrder.id as number, newOrderData);
+        const orderFromDb = await orderStore.getCompleteOrders(userId);
+
+        expect(orderFromDb).toEqual([
+            {
+                id: createdOrder.id,
+                user_id: userId,
+                status: false,
+                products: [
+                    {
+                        product_id: productId,
+                        quantity: 200,
+                        ...productData,
+                    },
+                ],
+            },
+        ]);
+
+        await deleteOrder(createdOrder.id as number);
+    });
+});
